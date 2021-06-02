8d42b64656f2f6eebc1b47dda63372777e780259
gpntbDev <84362997+gpntbDev@users.noreply.github.com>
2021-06-02 11:15:19 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
 
-const { PORT=3002, NODE_ENV } = process.env;
+const { PORT=5000, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
