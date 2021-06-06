86d89ad504ec5a2ade2b394fa2e89adcdff7a951
gpntbDev <84362997+gpntbDev@users.noreply.github.com>
2021-06-06 15:02:03 +0700

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -37,10 +37,10 @@
 </style>
 
 <svelte:head>
-	<title>Sapper project template </title>
+	<title>Sapper project DevOps template </title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<h1>Great success!!!</h1>
 <p>Great success!</p>
 
 <figure>
