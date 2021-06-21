1882a3045f1acdd76e9b2e59c918391d69266ca4
gpntbDev <84362997+gpntbDev@users.noreply.github.com>
2021-06-21 13:30:21 +0700

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -45,7 +45,7 @@
 
 <figure>
 	<img alt="Success Kid" src="{successkid}">
-	<figcaption>Have fun with Sapper!</figcaption>
+	<figcaption>Have fun with Sapper !!!!</figcaption>
 </figure>
 
 <p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
